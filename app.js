3b81f7523b4b136f825b9f23066bedb8dfd605c7
Your Name <you@example.com>
2018-04-09 14:58:23 +0530

    first commit

--- app.js
+++ app.js
@@ -8,6 +8,25 @@ var port = process.env.PORT || 3000;
 app.use(session({ secret: 'this-is-a-secret-token',resave: true, saveUninitialized: true, cookie: { maxAge: 60000 }}));
 redirect(app);
 //global.recentInput = "";
+// Add headers
+app.use(function (req, res, next) {
+
+    // Website you wish to allow to connect
+    res.setHeader('Access-Control-Allow-Origin', 'http://www.tele2.com');
+
+    // Request methods you wish to allow
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+    // Request headers you wish to allow
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+
+    // Set to true if you need the website to include cookies in the requests sent
+    // to the API (e.g. in case you use sessions)
+    res.setHeader('Access-Control-Allow-Credentials', true);
+
+    // Pass to next layer of middleware
+    next();
+})
 app.use(express.static(__dirname + '/public'));
 app.use(bodyParser.urlencoded({ extended: false })) 
 app.use(bodyParser.json());
